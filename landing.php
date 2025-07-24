81235b0dbb8aeecbad02ec34ff4c568af8e08f69
GiovanaSantoss <gigonzaga987@gmail.com>
2025-07-24 16:03:01 -0300

    Adicionei uma pagina de entrada

---
+++ landing.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Minha Biblioteca</title>
+  <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+</head>
+<body>
+  <div class="container">
+    <h1 class="titulo">BOOKLOVER</h1>
+    <p class="frase">“Porque cada história lida é um pedaço da gente escrito.”</p>
+    <div class="botoes">
+      <a href="login.php" class="botao">Login</a>
+      <a href="cadastro.php" class="botao">Cadastre-se</a>
+    </div>
+  </div>
+</body>
+</html>
